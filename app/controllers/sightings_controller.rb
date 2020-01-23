a4bfa0fd33a725122bc3f4e9a527907e21613d92
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-01-23 00:25:33 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -2,13 +2,13 @@ class SightingsController < ApplicationController
 
   def index
     sightings = Sighting.all
-    render json: 
+    render json:
     SightingSerializer.new(sightings).to_serialized_json
   end
 
   def show
     sighting = Sighting.find_by(id: params[:id])
-    render json: 
+    render json:
     SightingSerializer.new(sightings).to_serialized_json
   end
 end
